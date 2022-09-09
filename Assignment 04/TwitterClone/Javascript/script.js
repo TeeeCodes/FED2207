1969ef64f002743e338683746af3630ccf5bc667
TeeeCodes <109982438+TeeeCodes@users.noreply.github.com>
2022-09-09 07:38:57 -0400

    clone changes

--- Assignment 04/TwitterClone/Javascript/script.js
+++ Assignment 04/TwitterClone/Javascript/script.js
@@ -17,15 +17,6 @@ const Post = function (text, dateCreated, uid) {
 
 
 
-
-
-
-
-
-
-
-
-
 var settingsmenu = document.querySelector(".settings-menu");
 var darkBtn = document.getElementById("dark-btn");
 
@@ -36,7 +27,7 @@ function settingsMenuToggle(){
 darkBtn.onclick = function() {
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");
-
+    
     if(localStorage.getItem("theme") == "light") {
         localStorage.setItem("theme", "dark");
     }
@@ -57,3 +48,16 @@ else if(localStorage.getItem("theme") == "dark") {
 else {
     localStorage.setItem("theme", "light");
 }
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXVt-RsOc14ZX2YGFuIB-j4MVDOoNw2og",
+    authDomain: "twitter-clone-72847.firebaseapp.com",
+    projectId: "twitter-clone-72847",
+    storageBucket: "twitter-clone-72847.appspot.com",
+    messagingSenderId: "255051256392",
+    appId: "1:255051256392:web:4563fc1d98672ec1a7fe7d",
+    measurementId: "G-NMDF7VZ4JF"
+  };
+
+  const app = initializeApp(firebaseConfig);
